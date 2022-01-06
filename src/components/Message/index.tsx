2e689ad478345442643ccf01faa9295e11fb315f
yongfei huo <hyfpersonal@outlook.com>
2022-01-06 20:21:54 +0800

    Optimize: Notification component, clear change;

--- src/components/Message/index.tsx
+++ src/components/Message/index.tsx
@@ -119,8 +119,8 @@ function message() {
   notificationCreator((notification: any) => (notificationRef = notification), {
     rootElementId: "rc_message_root",
     className: styles.wrapper,
-    transitionName: "fade",
-    position: "leftCenter"
+    transitionName: "slide",
+    position: "leftTop"
   });
 
   function genWrappedChildenByType(
